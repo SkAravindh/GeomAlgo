2e95c2b89094b6b1fdb94b64f791c9fb19637e7c
Skaravindh <fliegen95@gmail.com>
2021-02-23 09:09:32 +0100

    added edge_length

--- codes/EdgeOrder.h
+++ codes/EdgeOrder.h
@@ -20,6 +20,7 @@ public:
 public:
     Point *p0;
     Point *p1;
+    bool isborder = false;
 
     friend std::ostream & operator << (std::ostream & out, const EdgeOrder &rhs) {
         out<<"OrderEdge "<<"P0 "<< *rhs.p0 <<" "<<"P1 "<< *rhs.p1 << " ";
